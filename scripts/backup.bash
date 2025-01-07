59004ff9c0a74aa7e19be49d98c261b0153e6c62
Daniel Merz <merzdanieldeveloper@gmail.com>
2025-01-07 23:50:02 +0100

    prune and compact borg backup

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -15,7 +15,9 @@ docker compose exec db mysqldump --single-transaction -u root -p$MYSQL_ROOT_PASS
 set +e
 
 echo Creating backup... THIS MAY TAKE FOR AGES!
+export BORG_PASSPHRASE
 borg create        \
+  --show-rc        \
   --stats          \
   --exclude-caches \
   rsync:borg::nextcloud-$d              \
@@ -23,9 +25,39 @@ borg create        \
   /mnt/data/cloud/./db_dump             \
   /mnt/data/cloud/./nextcloud/config    \
   /mnt/data/cloud/./nextcloud/data
+backup_exit=$?
 
-
-echo Activate Nextcloud maintenance mode...
+echo Deactivate Nextcloud maintenance mode...
 docker-compose exec -u www-data app php occ maintenance:mode --off
+
+echo Prune backups...
+borg prune \
+  --show-rc                     \
+  --progress                    \
+  --glob-archives 'nextcloud-*' \
+  --keep-daily=7                \
+  --keep-weekly=4               \
+  --keep-monthly=12             \
+  --keep-yearly=2               \
+  rsync:borg
+prune_exit=$?
+
+echo Compact backups...
+borg compact rsync:borg
+compact_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+global_exit=$(( compact_exit > global_exit ? compact_exit : global_exit ))
+
+if [ ${global_exit} -eq 0 ]; then
+    echo "Backup, Prune, and Compact finished successfully"
+elif [ ${global_exit} -eq 1 ]; then
+    echo "Backup, Prune, and/or Compact finished with warnings"
+else
+    echo "Backup, Prune, and/or Compact finished with errors"
+fi
+
+exit ${global_exit}
 
 )
